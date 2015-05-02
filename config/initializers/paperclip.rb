bb032342619a88d8f1f19ec35cd513ee124898af
shutdown23 <grant.david@live.com>
2015-05-02 18:45:23 -0400

    fixed initilizer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,4 @@
 
 
-Paperclip::Attachment.default_options[:url] = ':rails-apps-production.s3.amazonaws.com)'
+Paperclip::Attachment.default_options[:url] = 'http://rails-apps-production.s3.amazonaws.com)'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
